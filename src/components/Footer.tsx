20e64e770816f504915b849091e7a187c8157057
kaique <kai_que15@hotmail.com>
2025-09-01 21:57:51 -0300

    first corrigindo bugs 12

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
             <div id="footer" className="w-full flex flex-col items-center justify-center bg-[#F9F9F9] xl:flex-row mt-16 bg-white">
                 <div className="w-full max-w-screen-xl flex flex-col  border-b border-solid border-gray-100 pb-6 mb-4 md:flex-row md:gap-8 md:pb-8 md:mb-8 lg:gap-[38px] xl:flex-row xl:justify-between">
                     <div className="flex flex-1 mb-7 items-center justify-center lg:mb-0">
-                        <img className="" src="/assets/solar-fernanda3.svg" />
+                        <img className="" src="/assets/solar 1.png" />
                     </div>
                     <div className="flex order-3 flex-1 mb-7 items-center justify-center lg:order-2 lg:mb-0">
                         <div className="w-2/3">
@@ -26,7 +26,7 @@ export const Footer = () => {
                 </div>
 
             </div>
-            <div className="size-14 z-99 fixed bottom-6 right-6 lg:bottom-14 lg:right-14 animate-jump animate-infinite animate-duration-1000">
+            <div className="size-14 z-90 fixed bottom-6 right-6 lg:bottom-14 lg:right-14 animate-jump animate-infinite animate-duration-1000">
                 <a target="_blank" href="https://api.whatsapp.com/send?l=pt&phone=5582982220914">
                     <img className="w-full" src="/assets/icons-whatsapp1.svg" />
                 </a>
